b83d15c0b1bcb1e51f51cd851c20f87a8e3de375
Snards02 <154646208+Snards02@users.noreply.github.com>
2025-09-10 15:28:34 -0500

    prueba 2

---
+++ src/.env/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyAuHfjd93C1NtDcsdf_nWJxzBpTUC6cD5c",
+        authDomain: "websiteh2s.firebaseapp.com",
+        projectId: "websiteh2s",
+        storageBucket: "websiteh2s.firebasestorage.app",
+        messagingSenderId: "1001600455468",
+        appId: "1:1001600455468:web:d23bc584f4a52eb7051289"
+    }
+};
